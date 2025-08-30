90e73ce2f739034fd91061761035f8393589ba36
Codeonthebab <kjanny152@gmail.com>
2025-08-30 20:06:06 +0900

    Python.DBCreate.UserAPI.LoginAPI.TESTHttp
    Table구성, 유저 가입API, LoginAPI
    JWT 토큰 관리, bcrypt 회원가입 API

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt #Bcrypt
+from flask_bcrypt import Bcrypt
 import datetime
+import jwt
 
 # Flask 앱 생성 & CORS 설정
 app = Flask(__name__)
 CORS(app)
 bcrypt = Bcrypt(app)
+app.config['SECRET_KEY']='holyMoly_4d23krgwtf_holydoly_a1s11b32nbf' # return 부분은 복잡한 문자열로 만들어두는게 좋다
 
 #데이터베이스 설정 : DB(PostgreSQL)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://threadboard_user:EBjc41eLhJv5y1YadGTgm20MZ1xLpmVV@dpg-d2o8687diees73ema8c0-a.singapore-postgres.render.com/threadboard'
@@ -49,7 +51,7 @@ class Post (db.Model):
 
 
 # 회원가입 API
-@app.route('/register', methods=['POST'])
+@app.route('/userProc', methods=['POST'])
 def register():
     data = request.json
     username = data.get('username')
@@ -73,3 +75,32 @@ def register():
     db.session.commit()
 
     return jsonify({"message" : f"'{username}'이(가) 회원가입되었습니다."}), 201 #성공
+
+# 로그인 API
+@app.route('/login', methods=['POST'])
+def login() :
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    # 공란 기입하면 반환할 if문
+    if not username or not password:
+        return jsonify({"error" : "사용자의 이름, 비밀번호를 입력해주십시오."}), 400
+    
+    # DB에서 유저 찾기
+    user = User.query.filter_by(username=username).first()
+
+    # 주워온 유저 정보가 존재하고, 비밀번호는 일치하는지 확인하기
+    if user and bcrypt.check_password_hash(user.password, password):
+        # 비밀번호 일치하면 JWT 생성
+        payload = {
+            'user_id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1) #1시간 후 티켓 만료 시킴
+        }
+        token = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
+
+        return jsonify({"token": token}), 200
+    else:
+        # 인증 실패
+        return jsonify({"error": "잘못된 사용자, 비밀번호입니다."}), 401
+
