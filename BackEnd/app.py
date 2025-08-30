d663eda8d7fb6de3c860d9972661ae4374af9740
Codeonthebab <kjanny152@gmail.com>
2025-08-30 17:36:31 +0900

    Python.UserTable.Query.UserAPI
    User Create bcrypt Lib
    Password Hashed

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt #Bcrypt
 import datetime
 
 # Flask 앱 생성 & CORS 설정
 app = Flask(__name__)
 CORS(app)
+bcrypt = Bcrypt(app)
 
 #데이터베이스 설정 : DB(PostgreSQL)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://threadboard_user:EBjc41eLhJv5y1YadGTgm20MZ1xLpmVV@dpg-d2o8687diees73ema8c0-a.singapore-postgres.render.com/threadboard'
@@ -45,3 +47,29 @@ class Post (db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     ip = db.Column(db.String(45), nullable=False)
 
+
+# 회원가입 API
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+    email = data.get('email')
+
+    # 기본 유효성 검사 : 모든 필드 누락
+    if not username or not email or not password:
+        return jsonify({"Error" : "모든 필드를 입력해주세요."}), 400 #실패
+    
+    # 사용자 및 이메일 중복 확인하기
+    if User.query.filter_by(username=username).first() or User.query.filter_by(email=email).first():
+        return jsonify({"Error" : "이미 존재하는 사용자의 이름 또는 이메일입니다."}), 409 #실패
+    
+    # 비밀번호 해싱 시키기
+    hashed_password = bcrypt.generate_pasword_hash(password).decode('utf-8')
+    
+    # 사용자 생성 및 DB에 추가시킴
+    new_user = User(username=username, password=hashed_password, email=email)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"Message" : f"'{username}'이(가) 회원가입되었습니다."}), 201 #성공
