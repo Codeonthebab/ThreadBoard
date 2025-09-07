104b2ee4e9f4cccb8dfa901e33150c7d92da5b18
Codeonthebab <kjanny152@gmail.com>
2025-09-08 01:04:08 +0900

    [fix] 각 페이지 루트 값 변경, 로그인, 회원가입 버튼 생성
    회원 가입, 로그인 페이지 연결 완료
    기타 디자인 변경 필요

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,22 +1,33 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import "./App.css";
 import "./pages/insertThread.tsx";
-import MainPage from "./pages/mainpage.tsx";
+import Register from "./pages/RegisterPage.tsx";
+import MainPage from "./pages/MainPage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
 import LanguageSwitcher from "./contexts/LanguageSwitcher.tsx";
 
+
 function App() {
+   const { t } = useTranslation();
 
   return (
     <div className="App">
       <header className="app-header-bar">
+        <nav>
+          <Link to="/login" style={{ marginRight: '10px'}}>{t('login')}</Link>
+          <Link to="/signup">{t('signup')}</Link>
+        </nav>
         <LanguageSwitcher/>
       </header>
-      <body>
+      <main>
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/insert-thread" element={<insertThread />} />
+          <Route path="/signup" element={<Register/>}/>
+          <Route path="/login" element={<LoginPage/>}/>
         </Routes>
-      </body>
+      </main>
     </div>
   );
 }
