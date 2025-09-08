61634d9f89e4c152c7bb0d92fc058d434a992e1a
Codeonthebab <kjanny152@gmail.com>
2025-09-09 00:29:36 +0900

    [feat] useAuth 커스텀 훅으로 캡슐화, AuthContext
    다른 컴포넌트에서도 언제든 불러서 그 페이지의 인증 값으로 만들어둠

--- FrontEnd/src/contexts/AuthContext.tsx
+++ FrontEnd/src/contexts/AuthContext.tsx
@@ -12,32 +12,38 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
     const [token, setToken] = useState<string | null>(null);
 
+    // 컴포넌트가 처음 마운트 될 때, 한 번만 실행
     useEffect(() => {
         const storedToken = localStorage.getItem('authToken');
         if (storedToken) {
             setToken(storedToken);
         }
-    }, []);
+    }, []); // []는 중복 실행 방지할 때 씀
+    // 의존성 배열이 비어있어서 이 컴포넌트 실행될 때, localStorage에 토큰이 있는지 확인하는 용도로 사용
 
+    // 로그인 함수
     const login = (newToken: string) => {
-        setToken(newToken);
-        localStorage.setItem('authToken', newToken);
+        localStorage.setItem('authToken', newToken); // localStorage에 토큰 저장함
+         setToken(newToken); // 새로운 토큰 발급하고 리액트의 state에 상태 업데이트 함
     };
 
+    // 로그아웃 함수
     const logout = () => {
-        setToken(null);
-        localStorage.removeItem('authToken');
+        localStorage.removeItem('authToken'); // localStorage에서 토큰을 삭제함
+        setToken(null); // 로그인 함수에서 저장해놨던 뉴토큰을 이제 널로 상태 변경한 후 토큰 초기화
     };
 
-    const isAuthenticated = !!token;
+    // Context를 통해 전달할 값, "login, logout" 함수를 객체로 묶어놓음
+    const value = { token, login, logout };
 
     return (
-        <AuthContext.Provider value={{ token, login, logout, isAuthenticated }}>
+        <AuthContext.Provider value={{ value }}>
             {children}
         </AuthContext.Provider>
     );
 };
 
+// 훅을 커스텀해놓음 : 다른 컴포넌트에서 useAuth로 Context 값을 쉽게 쓸수 있게 캡슐화 시켜놓는 것
 export const useAuth = () => {
     const context = useContext(AuthContext);
     if (!context) {
