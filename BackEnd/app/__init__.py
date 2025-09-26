74a46dac979e094bbbdc11630574386904559cca
Codeonthebab <kjanny152@gmail.com>
2025-09-26 17:44:10 +0900

    [fix] 깃 주석 수정

--- BackEnd/app/__init__.py
+++ BackEnd/app/__init__.py
@@ -28,7 +28,7 @@ def create_app(config_class=Config):
     with app.app_context():
         db.create_all()
 
-    # 5. 블루프린트(routes) 등록
+    # 블루프린트(routes) 등록
     app.register_blueprint(auth_bp)
     app.register_blueprint(threads_bp)
 
