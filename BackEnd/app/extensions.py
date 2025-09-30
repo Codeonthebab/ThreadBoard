d23161f1fcf8657af99458dd2d48697479359537
Codeonthebab <kjanny152@gmail.com>
2025-09-30 15:51:07 +0900

    [feat] flask_migrate 초기화 객체 추가

--- BackEnd/app/extensions.py
+++ BackEnd/app/extensions.py
@@ -1,9 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 
 #데이터베이스 객체 생성
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+migrate = Migrate()
 
 """
 로컬에서 비밀번호 잃어버림... test1으로 비밀번호 수정
