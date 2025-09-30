ae1bbe7de83ea67987b2d7f363fe7c3ee44b46b4
Codeonthebab <kjanny152@gmail.com>
2025-09-30 14:48:30 +0900

    [feat] 알림 읽음 처리 API 추가

--- BackEnd/app/routes/notifications.py
+++ BackEnd/app/routes/notifications.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, jsonify
 from .auth import token_required
 from ..models import Notification
+from ..extensions import db
 
 notifications_bp = Blueprint('notifications', __name__)
 
+# 알림 목록 API
 @notifications_bp.route('/notifications', methods=['GET'])
 @token_required
 def get_notifications(current_user) :
@@ -21,3 +23,19 @@ def get_notifications(current_user) :
     } for notification in notifications]
 
     return jsonify (notifications_data), 200
+
+# 알림 목록에서 읽음 처리하는 is_read 상태로 변경하는 API
+@notifications_bp.route('/notifications/<int:notification_id>/read', methods=['POST'])
+@token_required
+def mark_notification_as_read (current_user, notification_id) :
+    notification = Notification.query.get(notification_id)
+    
+    if not notification :
+        return jsonify({'error' : '존재하지 않는 알림입니다.'}), 404
+    
+    
+    if not notification.is_read :
+        notification.is_read = True
+        db.session.commit()
+    
+    return jsonify({'message' : '알림을 읽음 처리했습니다.'}), 200
