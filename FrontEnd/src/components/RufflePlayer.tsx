bdead62e6c7479dafe7c7dc10c0db6656b5d6536
Codeonthebab <kjanny152@gmail.com>
2025-10-11 18:07:26 +0900

    [fix] 인스턴스를 종료했지만 오디오 종료되지 않는 문제 발견

--- FrontEnd/src/components/RufflePlayer.tsx
+++ FrontEnd/src/components/RufflePlayer.tsx
@@ -10,14 +10,25 @@ const RufflePlayer: React.FC <RufflePlayerProps> = ({ swfUrl }) => {
     const containerRef = useRef <HTMLDivElement> (null);
 
     useEffect (() => {
+        // 플레이어 인스턴스 저장할 변수
+        let player : any = null;
         const ruffle = (window as any).RufflePlayer;
 
         if (ruffle && containerRef.current) {
-            const player = ruffle.newest().createPlayer();
-            containerRef.current.innerHTML= '';
-            containerRef.current.appendChild(player);
+            player = ruffle.newest().createPlayer();
+            const container = containerRef.current;
+
+            container.innerHTML= '';
+            container.appendChild(player);
             player.load(swfUrl);
         }
+
+        return () => {
+            // 컴포넌트 없어지면 뒷정리하게끔 (clean-up)
+            if (player) {
+                player.remove();
+            }
+        };
     }, [swfUrl]); // swfUrl 갱신될 때마다 플레이어 다시 불러오게끔
     return <div ref={containerRef} style={{ width: '800px', height: '600px'}} />;
 };
